55a1df6472e42dadc1511c14acbb1061aa4be4a4
Brocier <jschoe04@gmail.com>
2018-01-22 14:20:28 -0500

    Have to chance the name everywhere damn it.

--- db/models/User.js
+++ db/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const {UserSchema} = require('../schema')
 
-const User = mongoose.model('Idea', UserSchema)
+const User = mongoose.model('User', UserSchema)
 
 module.exports = User
