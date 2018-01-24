b05c5c456ac0f78f92736f124929b45b3d3d3992
Brocier <jschoe04@gmail.com>
2018-01-24 10:49:02 -0500

    Added another user to the seeds file to see where the wrapper array was sitting

--- db/seeds.js
+++ db/seeds.js
@@ -6,9 +6,11 @@ mongoose.connect(process.env.MONGODB_URI)
 const User = require('./models/User.js')
 
 const adventurer = new User({name: 'Adventurer', description: 'The hero of this story'})
+const josh = new User({name: 'Brocier', description: 'My first account name'})
 
 User
   .remove({})
   .then(() => adventurer.save())
+  .then(() => josh.save())
   .then(() => console.log('Successful Save!!!'))
   .then(() => mongoose.connection.close())
