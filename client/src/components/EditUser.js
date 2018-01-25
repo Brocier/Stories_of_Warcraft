19a794605480a1322b65329c6223554cd51f44d9
Brocier <jschoe04@gmail.com>
2018-01-25 14:52:39 -0500

    working on an edit user form

---
+++ client/src/components/EditUser.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+// import {Redirect} from 'react-router'
+import {connect} from 'react-redux'
+import {editUser} from '../actions/actions.js'
+import styled from 'styled-components'
+
+const EditUserFormContainer = styled.div `
+  border: purple .5px solid;
+  input{
+    width: 50%;
+    height: 20px;
+  }
+`
+class UserForm extends Component {
+  state = {
+    userToEdit: {
+      name: ''
+    },
+    redirect: false
+  }
+
+  handleChange = (event) => {
+    const updatedUser = {
+      ...this.state.userToEdit
+    }
+    const inputField = event.target.name
+    const inputValue = event.target.value
+    updatedUser[inputField] = inputValue
+
+    this.setState({userToEdit: updatedUser})
+  }
+
+  handleButtonPress = () => {
+    this
+      .props
+      .editUser(this.state.userToEdit)
+    this.setState({
+      userToEdit: {
+        name: ""
+      }
+    })
+  }
+
+  render() {
+    return (
+      <EditUserFormContainer>
+        <div>
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.userToEdit.name}/>
+          <button onClick={this.handleButtonPress}>Edit this user</button>
+        </div>
+      </EditUserFormContainer>
+    )
+  }
+}
+
+export default connect(null, {editUser})(UserForm)
