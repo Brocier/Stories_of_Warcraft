873da1c70b99ede0fa6b3d4a7b0a7e2b9512d15a
Brocier <jschoe04@gmail.com>
2018-01-25 12:02:21 -0500

    adding quests and characters to josh and beth

--- db/seeds.js
+++ db/seeds.js
@@ -7,14 +7,35 @@ const User = require('./models/User.js')
 const Character = require('./models/Character.js')
 const Quest = require('./models/Quest.js')
 
-const pegi = new Character
+const beth = new User({name: 'SasquatchSocks', description: 'The hero of this story'})
+const pegi = new Character({name: "Pegi", description: "The deadliest and cutest dang panda you've ever seen."})
+const flowers = new Quest({name: "Kill the nasty man-eating flowers", description: "Sometimes Gardener Frank just needs to slow down."})
+pegi
+  .quests
+  .push(flowers)
+beth
+  .characters
+  .push(pegi)
 
-const adventurer = new User({name: 'SasquatchSocks', description: 'The hero of this story'})
 const josh = new User({name: 'Brocier', description: 'My first account name'})
+const rashtael = new Character({name: "Rashtael", description: "Totally BA warrior"})
+const mastiva = new Charcter({name: "Mastiva", description: "Druid AF"})
+const kobolds = new Quest({name: "Cull the Kobolds", description: "Pesky rats with candles. Kill em' all."})
+const dragon = new Quest({name: "Sands of Time", description: "How about we learn how to turn into a dragon?"})
+
+rashtael
+  .quests
+  .push(kobolds)
+mastiva
+  .quests
+  .push(dragon)
+josh
+  .characters
+  .push(rashtael, mastiva)
 
 User
   .remove({})
-  .then(() => adventurer.save())
+  .then(() => beth.save())
   .then(() => josh.save())
   .then(() => console.log('Successful Save!!!'))
   .then(() => mongoose.connection.close())
