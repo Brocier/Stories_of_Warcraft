65a2b4a6af81d6a6348899e502e6cc0a11626d05
Brocier <jschoe04@gmail.com>
2018-01-28 09:49:08 -0500

    Well, that's that. I can read my other two models thanks to .map() I guess I'll wait on messing around with this more until after I get more reps in react/redux

--- client/src/components/character/UserCharacterPage.js
+++ client/src/components/character/UserCharacterPage.js
@@ -1,16 +1,77 @@
 import React, {Component} from 'react'
-
+import {connect} from 'react-redux'
+import {getUsers} from '../../actions/actions.js'
+// Guess I'll try the mapstatetoprops option first
+import styled from 'styled-components'
+const UserCharacterPageContainer = styled.div `
+  border: red solid 1px;
+  `
 class UserCharacterPage extends Component {
+  componentWillMount() {
+    this
+      .props
+      .getUsers()
+  }
   render() {
+    const userId = this.props.match.params.userid
     return (
-      <div>
-        Hello from the character Page
+      <UserCharacterPageContainer>
         <div>
-          {this.props.match.url}
+          {this
+            .props
+            .users
+            .map((user, i) => {
+              if (user._id === userId) {
+                return (
+                  <div key={i}>
+                    <div>{user.name}</div>
+                    <div>{user.description}</div>
+                    <div>
+                      {user
+                        .characters
+                        .map((character, i) => {
+                          return (
+                            <div>
+                              <div>
+                                Character
+                              </div>
+                              <div>
+                                {character.name}
+                              </div>
+                              <div>
+                                {character.description}
+                              </div>
+                              <div>
+                                {character
+                                  .quests
+                                  .map((quest, i) => {
+                                    return (
+                                      <div>
+                                        Quest
+                                        <div>
+                                          {quest.name}
+                                        </div>
+                                        <div>
+                                          {quest.description}
+                                        </div>
+                                      </div>
+                                    )
+                                  })}
+                              </div>
+                            </div>
+                          )
+                        })}
+                    </div>
+                  </div>
+                )
+              }
+            })}
         </div>
-      </div>
+      </UserCharacterPageContainer>
     )
   }
 }
-
-export default UserCharacterPage
+const mapStateToProps = (state) => {
+  return {users: state.users}
+}
+export default connect(mapStateToProps, {getUsers})(UserCharacterPage)
